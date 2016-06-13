c80d206283ed3f5bb9ca30d09442a480089d7311
pirimiri <linhdo@umass.edu>
2016-06-13 14:11:02 +0700

    Update arrays.java

--- arrays.java
+++ arrays.java
@@ -65,11 +65,11 @@ public int[] evenOdd(int[] nums) {
 }
 public int[] zeroMax(int[] nums) {
   
-  Return a version of the given array where each zero value in the array is replaced by the largest odd value to the right of the zero 
-  in the array. If there is no odd value to the right of the zero, leave the zero as a zero.
-  zeroMax([0, 5, 0, 3]) → [5, 5, 3, 3]
-  zeroMax([0, 4, 0, 3]) → [3, 4, 3, 3]
-  zeroMax([0, 1, 0]) → [1, 1, 0]
+  // Return a version of the given array where each zero value in the array is replaced by the largest odd value to the right of the zero 
+  // in the array. If there is no odd value to the right of the zero, leave the zero as a zero.
+  // zeroMax([0, 5, 0, 3]) → [5, 5, 3, 3]
+  // zeroMax([0, 4, 0, 3]) → [3, 4, 3, 3]
+  // zeroMax([0, 1, 0]) → [1, 1, 0]
   
   int max = 0;
   for (int i = nums.length-1; i >= 0; i--) { //iterate backwards b/c zero only gets max to its right
