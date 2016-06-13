84331ec5bcfa27cf1b0bf9f5fab0a78edd1da674
pirimiri <linhdo@umass.edu>
2016-06-13 21:50:02 +0700

    Update strings.java

--- strings.java
+++ strings.java
@@ -126,3 +126,13 @@ public String getSandwich(String str) {
   return str.substring(start+5, end);
   
 }
+public String zipZap(String str) {
+// Look for patterns like "zip" and "zap" in the string -- length-3, starting with 'z' and ending with 'p'. 
+// Return a string where for all such words, the middle letter is gone, so "zipXzap" yields "zpXzp".
+
+  for (int i=0; i < str.length() - 2; i++) 
+    if (str.charAt(i) == 'z' && str.charAt(i+2) == 'p')
+      str = str.substring(0, i+1) + str.substring(i+2); // nice trick to delete [i+1] in str
+  return str;
+}
+
