b01ed1088559e9bd3c270c531bfdf280c7eebfca
pirimiri <linhdo@umass.edu>
2016-06-17 00:27:02 +0700

    Update arrays.java

--- arrays.java
+++ arrays.java
@@ -78,5 +78,25 @@ public int[] zeroMax(int[] nums) {
   }
   return nums;
 }
+public int maxSpan(int[] nums) {
+// Consider the leftmost and righmost appearances of some value in an array. We'll say that the "span" is the number of elements between 
+// the two inclusive. A single value has a span of 1. Returns the largest span found in the given array. (Efficiency is not a priority.)
 
-
+// maxSpan([1, 2, 1, 1, 3]) → 4
+// maxSpan([1, 4, 2, 1, 4, 1, 4]) → 6
+// maxSpan([1, 4, 2, 1, 4, 4, 4]) → 6
+ 
+  if (nums.length == 0) return 0;
+  int maxSpan = 1;
+  for (int i=0; i<nums.length; i++) // step through all elts 
+  // (Efficiency is not a priority, so although we bump into the middle elts, this is the way to solve the problem)
+  
+    for (int j=nums.length-1; j>i; j--) 
+      if (nums[j] == nums[i]) { // rightmost appearance
+        int count = (j - i) + 1; // calculate this span
+        if (count > maxSpan) maxSpan = count; // max so far
+        break;
+      }
+  return maxSpan;
+  
+}
